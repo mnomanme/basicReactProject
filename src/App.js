7a9708cadac9d6374575deee78576a0181e2d287
mnomanme <abdullah.noman083@gmail.com>
2022-11-23 00:20:23 +0600

    grocery bud structure and add items complete :rocket:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
+import { useState } from 'react';
+import { Alert } from './components/Alert';
+import { List } from './components/List';
+
 const App = () => {
+	const [name, setName] = useState('');
+	const [list, setList] = useState([]);
+	const [isEditing, setIsEditing] = useState(false);
+	const [editId, setEditID] = useState(null);
+	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.table('hello');
+		if (!name) {
+			// display alert
+		} else if (name && isEditing) {
+			// deal with edit
+		} else {
+			// show alert
+			const newItem = { id: new Date().getTime().toString(), title: name };
+			setList([...list, newItem]);
+			setName('');
+		}
+	};
+
 	return (
 		<>
-			<h2>grocery bud</h2>
+			<section className="section-center">
+				<form className="grocery-form" onSubmit={handleSubmit}>
+					{alert.show && <Alert />}
+					<h3>grocery bud</h3>
+					<div className="form-control">
+						<input
+							type="text"
+							name="name"
+							id="name"
+							className="grocery"
+							placeholder="e.g. potatoes"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+						<button type="submit" className="submit-btn">
+							{isEditing ? 'edit' : 'submit'}
+						</button>
+					</div>
+				</form>
+				{list.length > 0 && (
+					<div className="grocery-container">
+						<List items={list} />
+						<button className="clear-btn">clear items</button>
+					</div>
+				)}
+			</section>
 		</>
 	);
 };
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,26 @@
 import { FaEdit, FaTrash } from 'react-icons/fa';
 
-export const List = () => {
+export const List = ({ items }) => {
 	return (
 		<>
-			<h3>List</h3>
+			<div className="grocery-list">
+				{items.map((item) => {
+					const { id, title } = item;
+					return (
+						<article key={id} className="grocery-item">
+							<p className="title">{title}</p>
+							<div className="btn-container">
+								<button type="button" className="edit-btn">
+									<FaEdit />
+								</button>
+								<button type="button" className="delete-btn">
+									<FaTrash />
+								</button>
+							</div>
+						</article>
+					);
+				})}
+			</div>
 		</>
 	);
 };
