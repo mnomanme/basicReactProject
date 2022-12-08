b18ac2520e2c8d53e63e562eb16d4b2bd52ff643
mnomanme <abdullah.noman083@gmail.com>
2022-12-08 19:59:38 +0600

    submenu location changeable :rocket:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,13 @@ export const Navbar = () => {
 	const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
 	const displaySubmenu = (e) => {
-		console.log('hello world');
-		openSubmenu();
+		// console.log(e.target);
+		const page = e.target.textContent;
+		const tempBtn = e.target.getBoundingClientRect();
+		const center = (tempBtn.left + tempBtn.right) / 2;
+		const bottom = tempBtn.bottom - 3;
+		// console.log(tempBtn);
+		openSubmenu(page, { center, bottom });
 	};
 
 	return (
