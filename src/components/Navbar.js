ad49de7ae8302caa3e92d2d723836365a5a65fde
mnomanme <abdullah.noman083@gmail.com>
2022-12-08 17:12:01 +0600

    add navbar :rocket:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,33 @@
 import logo from '../assests/logo.svg';
 import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from '../context';
 
 export const Navbar = () => {
+	const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 	return (
 		<>
-			<h2>navbar component</h2>
+			<nav className="nav">
+				<div className="nav-center">
+					<div className="nav-header">
+						<img src={logo} alt="stripe" className="nav-logo" />
+						<button className="btn toggle-btn" onClick={openSidebar}>
+							<FaBars />
+						</button>
+					</div>
+					<ul className="nav-links">
+						<li>
+							<button className="link-btn">products</button>
+						</li>
+						<li>
+							<button className="link-btn">developers</button>
+						</li>
+						<li>
+							<button className="link-btn">company</button>
+						</li>
+					</ul>
+					<button className="btn signin-btn">Signin</button>
+				</div>
+			</nav>
 		</>
 	);
 };
