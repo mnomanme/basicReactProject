07384fb49e4602ca28150112057f82a70dc76d5e
mnomanme <abdullah.noman083@gmail.com>
2022-12-08 23:23:24 +0600

    hide submenu navbar :fire:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,9 +15,13 @@ export const Navbar = () => {
 		openSubmenu(page, { center, bottom });
 	};
 
+	const handleSubmenu = (e) => {
+		if (!e.target.classList.contains('link-btn')) closeSubmenu();
+	};
+
 	return (
 		<>
-			<nav className="nav">
+			<nav className="nav" onMouseOver={handleSubmenu}>
 				<div className="nav-center">
 					<div className="nav-header">
 						<img src={logo} alt="stripe" className="nav-logo" />
