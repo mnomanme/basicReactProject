6cd767b4b2b8e953e8ec9b29543fa0d83e6881b7
mnomanme <abdullah.noman083@gmail.com>
2022-12-08 17:49:09 +0600

    hero update :fire:

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,26 @@ import phoneImg from '../assests/phone.svg';
 import { useGlobalContext } from '../context';
 
 export const Hero = () => {
-	const data = useGlobalContext();
-	console.log(data);
+	const { closeSubmenu } = useGlobalContext();
 
 	return (
 		<>
-			<h2>hero component</h2>
+			<section className="hero">
+				<div className="hero-center">
+					<article className="hero-info">
+						<h1>payments infrastructure for the internet</h1>
+						<p>
+							Millions of compaines of all sizes-from startups to Fortune
+							500s-use Stripe's software and APIs to accept payments, send
+							payouts, and manage their businesses online.
+						</p>
+						<button className="btn">Start Now</button>
+					</article>
+					<article className="hero-images">
+						<img src={phoneImg} className="phone-img" alt="phone-img" />
+					</article>
+				</div>
+			</section>
 		</>
 	);
 };
